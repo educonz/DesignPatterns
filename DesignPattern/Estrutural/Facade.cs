e155b486367de517f8ec1be4abca722dfc795ca6
conz <conz.eduardo@hotmail.com>
2017-09-18 23:37:41 -0300

    Mais alguns designers

---
+++ DesignPattern/Estrutural/Facade.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Estrutural
+{
+
+    public class SolicitacaoFreteFacade
+    {
+        private readonly IConvocaoTransportadora _convocaTransportadora;
+        private readonly IControleEmail _controleEmail;
+        private readonly IControleSessao _controleSessao;
+        private readonly ILog _log;
+
+        public SolicitacaoFreteFacade(
+            IConvocaoTransportadora convocaTransportadora,
+            IControleEmail controleEmail,
+            IControleSessao controleSessao,
+            ILog log)
+        {
+            _convocaTransportadora = convocaTransportadora;
+            _controleEmail = controleEmail;
+            _controleSessao = controleSessao;
+            _log = log;
+        }
+
+        public void PrepararEIniciarSessao()
+        {
+            _log.RegistraLog("Iniciando processo de inicio de sessão!");
+            _convocaTransportadora.BuscarTransportadoras();
+            _convocaTransportadora.SelecionarRankingTransportadoras();
+            _controleEmail.EnviarEmailParaTransportadoras();
+            _controleSessao.IniciarSessao();
+            _log.RegistraLog("Sessão iniciada com sucesso!");
+        }
+    }
+
+    public interface ILog
+    {
+        void RegistraLog(string log);
+    }
+
+    public interface IControleSessao
+    {
+        void IniciarSessao();
+    }
+
+    public interface IControleEmail
+    {
+        void EnviarEmailParaTransportadoras();
+    }
+
+    public interface IConvocaoTransportadora
+    {
+        void BuscarTransportadoras();
+        void SelecionarRankingTransportadoras();
+    }
+}
---
+++ DesignPattern/Estrutural/Proxy.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Estrutural
+{
+    public interface IConta
+    {
+        decimal ConsultarSaldo();
+        void Depositar(decimal valor);
+        void Sacar(decimal valor);
+    }
+
+    public class ContaPoupanca : IConta
+    {
+        public decimal ConsultarSaldo()
+        {
+            return 1000;
+        }
+
+        public void Depositar(decimal valor)
+        {
+        }
+
+        public void Sacar(decimal valor)
+        {
+        }
+    }
+
+    public class ContaPoupancaProxy : IConta
+    {
+        private readonly ContaPoupanca _contaPoupanca;
+
+        public ContaPoupancaProxy(ContaPoupanca contaPoupanca)
+        {
+            _contaPoupanca = contaPoupanca;
+        }
+
+        public decimal ConsultarSaldo()
+        {
+            return _contaPoupanca.ConsultarSaldo();
+        }
+
+        public void Depositar(decimal valor)
+        {
+            if (valor > 2000)
+                throw new Exception("Valor não permitido para depósito!");
+
+            _contaPoupanca.Depositar(valor);
+        }
+
+        public void Sacar(decimal valor)
+        {
+            if (valor > 2000)
+                throw new Exception("Valor não permitido para saque!");
+
+            _contaPoupanca.Sacar(valor);
+        }
+    }
+}
